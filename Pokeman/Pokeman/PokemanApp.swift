90cc951ca1a743c073cde5943acf5eb10a0da230
vj-developer <vjdeveloper2020@gmail.com>
2023-09-20 14:35:32 +0530

    Initial Commit

---
+++ Pokeman/Pokeman/PokemanApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokemanApp.swift
+//  Pokeman
+//
+//  Created by Iyyappan on 20/09/23.
+//
+
+import SwiftUI
+
+@main
+struct PokemanApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
